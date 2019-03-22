9711b2dc2b086bed9aa1d9fde154e96f86f52bd7
Josh Long <josh@joshlong.com>
2019-03-22 16:06:41 +0100

    added a test for the Thymeleaf view that demonstrates the publisher for the fragment.

--- webflux/src/test/java/rsb/views/CustomerControllerTest.java
+++ webflux/src/test/java/rsb/views/CustomerControllerTest.java
@@ -3,62 +3,88 @@ package rsb.views;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.FileCopyUtils;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import rsb.utils.IntervalMessageProducer;
 
-@Log4j2
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 public class CustomerControllerTest {
 
-	private int take = 10;
-
-	@Autowired
-	private WebTestClient client;
-
-	private Flux<String> ticker() {
-		return this.client
-			.get()
-			.uri("/ticker.php")
-			.exchange()
-			.returnResult(String.class)
-			.getResponseBody();
-	}
-
-	private Flux<String> stream() {
-		return this.client
-			.get()
-			.uri("/stream.php")
-			.accept(MediaType.TEXT_EVENT_STREAM)
-			.exchange()
-			.returnResult(String.class)
-			.getResponseBody()
-			.take(this.take);
-	}
-
-	Flux<String> produce() {
-		return stream().doOnNext(html -> log.info("html: " + html));
-	}
-
 	@Test
-	public void streamSseHtmlEndpoint() throws Exception {
+	public void useTemplateEngine() throws Exception {
 
+		var templateResolver = new ClassLoaderTemplateResolver() {
+			{
+				setPrefix("/templates/");
+				setSuffix(".html");
+			}
+		};
 
-		StepVerifier
-			.withVirtualTime(this::produce)
-			.expectNextMatches(html -> html.contains("<div") || html.contains("#"))
-			.verifyComplete();
+		var springWebFluxTemplateEngine = new SpringWebFluxTemplateEngine() {
+			{
+				setTemplateResolver(templateResolver);
+			}
+		};
 
-//		stream().subscribe(log::info);
-		Thread.sleep(1000 * 10);
+		var bufferFactory = new DefaultDataBufferFactory();
 
+		var htmlMediaType = MediaType.TEXT_HTML;
+		var charset = Charset.forName("UTF-8");
+
+		var template = "ticker";
+		var selectors = new HashSet<String>();
+
+		var take = 2;
+		var context = new Context();
+		context.setVariable("updates", new ReactiveDataDriverContextVariable(
+				IntervalMessageProducer.produce().take(take), 1));
+
+		StepVerifier.Step<String> stringStep = StepVerifier
+				.create(Flux.from(springWebFluxTemplateEngine.processStream(template,
+						selectors, context, bufferFactory, htmlMediaType, charset))
+						.map(this::bufferAsString))
+				.expectNextMatches(html -> html.contains("<title>Tick Tock</title>"));
+
+		for (int i = 0; i < take; i++) {
+			int capture = 1 + i;
+			stringStep = stringStep
+					.expectNextMatches(html -> html.contains("# " + capture));
+		}
+
+		stringStep.expectNextMatches(html -> html.contains("</html>"));
+
+		stringStep.verifyComplete();
 
 	}
+
+	private String bufferAsString(DataBuffer dataBuffer) {
+		try (InputStream is = dataBuffer.asInputStream();
+				Reader ir = new InputStreamReader(is)) {
+			return FileCopyUtils.copyToString(ir);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
