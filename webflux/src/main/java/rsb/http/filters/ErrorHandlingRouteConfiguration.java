483895d4c3859882a5df2a86cb2e56a0b1e3e37e
Josh Long <josh@joshlong.com>
2019-03-28 09:24:50 -0400

    routes

---
+++ webflux/src/main/java/rsb/http/filters/ErrorHandlingRouteConfiguration.java
@@ -0,0 +1,51 @@
+package rsb.http.filters;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+class ErrorHandlingRouteConfiguration {
+
+	@Bean
+	RouterFunction<ServerResponse> errors() {
+		var productIdPathVariable = "productId";
+		return route().GET("/products/{" + productIdPathVariable + "}", request -> {
+			var productId = request.pathVariable(productIdPathVariable);
+			if (!Set.of("1", "2").contains(productId)) {
+				return ServerResponse.ok().syncBody(new Product(productId));
+			}
+			else {
+				return Mono.error(new ProductNotFoundException(productId));
+			}
+		}).filter((request, next) -> next.handle(request) //
+				.onErrorResume(ProductNotFoundException.class,
+						pnfe -> ServerResponse.notFound().build()))
+				.build();
+	}
+
+}
+
+@Data
+@RequiredArgsConstructor
+class Product {
+
+	private final String id;
+
+}
+
+@Data
+@RequiredArgsConstructor
+class ProductNotFoundException extends RuntimeException {
+
+	private final String productId;
+
+}
---
+++ webflux/src/test/java/rsb/http/filters/ErrorHandlingRouteConfigurationTest.java
@@ -0,0 +1,30 @@
+package rsb.http.filters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
+@Import(ErrorHandlingRouteConfiguration.class)
+public class ErrorHandlingRouteConfigurationTest {
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	public void valid() {
+		this.client.get().uri("/products/4").exchange().expectStatus().isOk().expectBody()
+				.jsonPath(".id").isEqualTo("4");
+	}
+
+	@Test
+	public void invalid() {
+		this.client.get().uri("/products/1").exchange().expectStatus().isNotFound();
+	}
+
+}
