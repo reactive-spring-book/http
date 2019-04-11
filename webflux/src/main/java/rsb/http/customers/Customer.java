266617eac2e9e55d95243ceb57a2c58689813e24
Josh Long <josh@joshlong.com>
2019-04-11 19:04:44 +0400

    customers

--- webflux/src/main/java/rsb/http/customers/Customer.java
+++ webflux/src/main/java/rsb/http/customers/Customer.java
@@ -3,24 +3,15 @@ package rsb.http.customers;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @NoArgsConstructor
 class Customer {
 
-	private String id;
-
-	private String name;
+	private String id, name;
 
 	Customer(String i, String n) {
 		this.id = i;
 		this.name = n;
-	}
-
-	Customer(UUID uid, String name) {
-		this.id = uid.toString();
-		this.name = name;
 	}
 
 	Customer(String name) {
