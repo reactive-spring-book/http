d6673bb0b742f89776f29e77bf7822916ff0490b
Josh Long <josh@joshlong.com>
2019-05-08 16:30:03 +0100

    up

--- webclient/src/main/java/rsb/client/HttpClientApplication.java
+++ webclient/src/main/java/rsb/client/HttpClientApplication.java
@@ -1,21 +1,16 @@
 package rsb.client;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @Log4j2
 @SpringBootApplication
 public class HttpClientApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.port", "0");
-
-		SpringApplication.run(HttpClientApplication.class, args);
-	}
-
-	static void accept(Greeting greeting) {
-		log.info("greeting: " + greeting);
+		new SpringApplicationBuilder().sources(HttpClientApplication.class)
+				.profiles("client").run(args);
 	}
 
 }
