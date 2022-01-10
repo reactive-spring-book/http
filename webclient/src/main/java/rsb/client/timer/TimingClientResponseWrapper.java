ea2463380db427df8776dc93dc26d39d7de8dcfc
Josh Long <josh@joshlong.com>
2022-01-09 19:14:43 -0800

    oops

--- webclient/src/main/java/rsb/client/timer/TimingClientResponseWrapper.java
+++ webclient/src/main/java/rsb/client/timer/TimingClientResponseWrapper.java
@@ -14,58 +14,58 @@ import java.time.Instant;
 @Slf4j
 class TimingClientResponseWrapper extends ClientResponseWrapper {
 
-	TimingClientResponseWrapper(ClientResponse delegate) {
-		super(delegate);
-	}
+    TimingClientResponseWrapper(ClientResponse delegate) {
+        super(delegate);
+    }
 
-	private void start() {
-		log.info("start @ " + Instant.now().toString());
-	}
+    private void start() {
+        log.info("start @ " + Instant.now().toString());
+    }
 
-	private void stop() {
-		log.info("stop @ " + Instant.now().toString());
-	}
+    private void stop() {
+        log.info("stop @ " + Instant.now().toString());
+    }
 
-	// <1>
-	private <T> Mono<T> log(Mono<T> c) {
-		return c.doOnSubscribe(s -> start()).doFinally(s -> stop());
-	}
+    // <1>
+    private <T> Mono<T> log(Mono<T> c) {
+        return c.doOnSubscribe(s -> start()).doFinally(s -> stop());
+    }
 
-	private <T> Flux<T> log(Flux<T> c) {
-		return c.doOnSubscribe(s -> start()).doFinally(s -> stop());
-	}
+    private <T> Flux<T> log(Flux<T> c) {
+        return c.doOnSubscribe(s -> start()).doFinally(s -> stop());
+    }
 
-	// <2>
-	@Override
-	public <T> T body(BodyExtractor<T, ? super ClientHttpResponse> extractor) {
-		T body = super.body(extractor);
-		if (body instanceof Flux) {
-			return (T) log((Flux) body);
-		}
-		if (body instanceof Mono) {
-			return (T) log((Mono) body);
-		}
-		return body;
-	}
+    // <2>
+    @Override
+    public <T> T body(BodyExtractor<T, ? super ClientHttpResponse> extractor) {
+        T body = super.body(extractor);
+        if (body instanceof Flux f) {
+            return (T) log(f);
+        }
+        if (body instanceof Mono m) {
+            return (T) log(m);
+        }
+        return body;
+    }
 
-	@Override
-	public <T> Mono<T> bodyToMono(Class<? extends T> elementClass) {
-		return log(super.bodyToMono(elementClass));
-	}
+    @Override
+    public <T> Mono<T> bodyToMono(Class<? extends T> elementClass) {
+        return log(super.bodyToMono(elementClass));
+    }
 
-	@Override
-	public <T> Mono<T> bodyToMono(ParameterizedTypeReference<T> elementTypeRef) {
-		return log(super.bodyToMono(elementTypeRef));
-	}
+    @Override
+    public <T> Mono<T> bodyToMono(ParameterizedTypeReference<T> elementTypeRef) {
+        return log(super.bodyToMono(elementTypeRef));
+    }
 
-	@Override
-	public <T> Flux<T> bodyToFlux(Class<? extends T> elementClass) {
-		return log(super.bodyToFlux(elementClass));
-	}
+    @Override
+    public <T> Flux<T> bodyToFlux(Class<? extends T> elementClass) {
+        return log(super.bodyToFlux(elementClass));
+    }
 
-	@Override
-	public <T> Flux<T> bodyToFlux(ParameterizedTypeReference<T> elementTypeRef) {
-		return log(super.bodyToFlux(elementTypeRef));
-	}
+    @Override
+    public <T> Flux<T> bodyToFlux(ParameterizedTypeReference<T> elementTypeRef) {
+        return log(super.bodyToFlux(elementTypeRef));
+    }
 
 }
